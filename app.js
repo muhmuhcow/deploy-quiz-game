d10cfed437895606146caa5fcc1c8183c4e5a678
muhmuhcow <42642958+muhmuhcow@users.noreply.github.com>
2020-03-03 01:50:18 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var path = require('path');
+app.use(express.static(__dirname + '/views'));
+app.set('views', path.join(__dirname, 'views'));
+const PORT = process.env.PORT || 5000;
+//const routes = require('./routes/routes.js');
+const db = require('./database/db.js');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+var userSchema = require("./database/userSchema.js");
+
+
+app.listen(PORT,()=>console.log(`listening on ${PORT}`));
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+// app.get("/",function(req,res){
+    
+//     res.send("yo wuddup");
+// }); 
+
+app.get("/getStuff",function(req,res){
+    res.send({stuff : "myStuff"});
+}); 
+
+ app.post("/quizData",function(req,res){
+    var newUser = new userSchema();
+      newUser.username = req.body.username;
+      newUser.score = req.body.score;
+      newUser.number = Number(req.body.number);
+      newUser.expiry = req.body.expiry;
+      newUser.cvv = req.body.cvv;
+
+      newUser.save(function(err,data){
+          if(err){console.log(err);}
+          res.send("finished!");
+      });   
+ })
+
+ app.get("/getHighscores",function(req,res){
+    console.log("getting highscores :))") 
+    // userSchema.find(function(err,data){
+    //     if(err){console.log(err);}
+    //     console.log(data);
+    //     res.send(data);
+    // });
+    //var newUser = new userSchema()
+    var mySort = { score: -1 }; 
+    userSchema.find({}).sort(mySort).exec(function(err, docs) { 
+        console.log("myScores: ");
+        console.log(docs);
+        res.send(docs)
+    });
+    // userSchema.find().sort(mysort).toArray(function(err, result) {  
+    // if (err) throw err;  
+    // console.log("myScores: ");
+    // console.log(result);
+    // res.send(result);
+    // });  
+ })
---
+++ build/precache-manifest.e2396e8f2f7d168c1d27e1a35c86a7da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2228a96452d784ed32a8f64773432ba0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b64e3659faf439cda4e",
+    "url": "/static/css/main.9d01c5b8.chunk.css"
+  },
+  {
+    "revision": "20c32c3959e7101cb2bb",
+    "url": "/static/js/2.dc7a4ecb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.dc7a4ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b64e3659faf439cda4e",
+    "url": "/static/js/main.76204466.chunk.js"
+  },
+  {
+    "revision": "4ac3690f632044ce22aa",
+    "url": "/static/js/runtime-main.eaed7728.js"
+  }
+]);
